2e515c55b12c6b9f6e9f1cb25fa94dcb2358909d
DT-GAMER <abakpad82@gmail.com>
2022-12-01 13:40:37 +0100

    3-print_number.py

---
+++ 0x00-python-hello_world/my-thought.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+for i in range(1, 10):
+    if i <=  5:
+        print('AAAAAAHHHHHHH')
+    elif  i >= 6 and i <= 8:
+        print('WHERE ARE ALL THE BRACKETS BAYIII?????')
+    else:
+        print('HOW DO YOU PEOPLE READ THIS SYNTAX EASILY, BECAUSE KO YE MI OOOO')
+         
