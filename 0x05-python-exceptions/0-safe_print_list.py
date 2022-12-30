fe2c28ec2aef1a03c3f6652c03a42169225c4947
DT-GAMER <abakpad82@gmail.com>
2022-12-30 11:38:13 -0800

    0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    printed_elements = 0
+    for i in range(x):
+        try:
+            print('{}', .format(my_list=[i]), end=' ')
+            printed_elements =+ 1
+            print()
+        except indexerror:
+            pass
+        print()
+        return (printed_elements)
