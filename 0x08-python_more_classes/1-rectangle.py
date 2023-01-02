fb123aa9dc0ef4f843e2ad4051ef20209c80b882
DT-GAMER <abakpad82@gmail.com>
2023-01-02 14:56:13 -0800

    0x08-python_more_classes

---
+++ 0x08-python_more_classes/1-rectangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+class rectangle:
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+
+    @property
+    def height(self):
+        return self._height
+
+    @height.sette
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >=0')
+        self._height = value
+
