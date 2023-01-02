12d1e51ecf2f6bbf15bcb23038473c825a0b276b
DT-GAMER <abakpad82@gmail.com>
2023-01-02 15:05:36 -0800

    alx-higher_level_programming

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
+
+
 def safe_print_list(my_list=[], x=0):
-    printed_elements = 0
-    for i in range(x):
-        try:
+    try:
+        printed_elements = 0 
+        for i in range(x):
             print('{}', .format(my_list=[i]), end=' ')
-            printed_elements =+ 1
+            printed_elements += 1
             print()
-        except indexerror:
-            pass
-        print()
-        return (printed_elements)
+    except:
+        pass
+    print()
+    return (printed_elements)
+
--- 101-square_matrix_map.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-def square_matrix_map(matrix=[]):
-    return (list(map(lambda r: list(map(lambda x: x**2, r)), matrix)))
