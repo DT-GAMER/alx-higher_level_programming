9495146292d477a446de3a87b39849f552116969
Abakpa Dominic <abakpad82@gmail.com>
2023-01-03 05:01:10 +0100

    Update 3-rectangle.py

--- 0x08-python_more_classes/3-rectangle.py
+++ 0x08-python_more_classes/3-rectangle.py
@@ -1,45 +1,125 @@
-#!/usr/bin/python3
-class rectangle:
-        def __init__(self, width=0, height=0):
-            self.width = width
-            self.height = height
+#!/usr/bin/python
+class Rectangle:
 
-        @property
-        def width(self):
-            return self.__width
+    def __init__(self, width=0, height=0):
+
+        """Initialize a rectangle with optional width and height.
+
+        If no width or height are provided, the default value is 0.
+
+        """
+
+        self.width = width
+
+        self.height = height
+
+    
+
+    @property
+
+    def width(self):
+
+        """Return the width of the rectangle."""
+
+        return self._width
+
+    
+
+    @width.setter
+
+    def width(self, value):
+
+        """Set the width of the rectangle.
+
+        Raise a TypeError if the width is not an integer.
+
+        Raise a ValueError if the width is negative.
+
+        """
+
+        if not isinstance(value, int):
+
+            raise TypeError("width must be an integer")
+
+        if value < 0:
+
+            raise ValueError("width must be >= 0")
+
+        self._width = value
+
+    
+
+    @property
+
+    def height(self):
+
+        """Return the height of the rectangle."""
+
+        return self._height
+
+    
+
+    @height.setter
+
+    def height(self, value):
+
+        """Set the height of the rectangle.
+
+        Raise a TypeError if the height is not an integer.
+
+        Raise a ValueError if the height is negative.
+
+        """
+
+        if not isinstance(value, int):
+
+            raise TypeError("height must be an integer")
+
+        if value < 0:
+
+            raise ValueError("height must be >= 0")
+
+        self._height = value
+
+    
+
+    def area(self):
+
+        """Return the area of the rectangle."""
+
+        return self.width * self.height
+
+    
+
+    def perimeter(self):
+
+        """Return the perimeter of the rectangle.
+
+        If either the width or the height is 0, return 0.
+
+        """
+
+        if self.width == 0 or self.height == 0:
+
+            return 0
+
+        return 2 * (self.width + self.height)
+
+    
+
+    def __str__(self):
+
+        """Return a string representation of the rectangle.
+
+        If either the width or the height is 0, return an empty string.
+
+        """
+
+        if self.width == 0 or self.height == 0:
+
+            return ""
+
+        return "\n".join("#" * self.width for _ in range(self.height))
 
 
-        @width.setter
-        def width(self, value):
-        
-            if not isinstance(value, int):
-                TypeError('width must be an integer')        
-            if value < 0:
-                raise ValueError('width must be >= 0')                                                                                                                              self.__width = value
-
-
-        @property                                                                                                                                                               def height(self):
-            return self.__height
-
-
-                                                                                                                                                                                @height.setter                                                                                                                                                          def height(self, value):                            
-            if not isinstance(value, int):
-                TypeError('height must be an integer')
-            if value < 0:
-                raise ValueError('width must be >= 0')
-            self.__height = value
-
-
-        def area(self):
-            return self._width * self._height
-
-        def perimeter(self):
-            if self.__width == 0 or self.__height == 0:
-                return 0  
-            return 2 * (self.__width + self.__height)
-
-        def __str__(self):
-            if self.width == 0 or self.height == 0:
-                return ""
-            return "/n".join("#" * self.width for _ in range(self.height))
 
