e50a78ea40d5830c49965dfbdc2e0bbe61feff08
DT-GAMER <abakpad82@gmail.com>
2023-01-07 13:42:11 -0800

    0x03-python-data_structures

--- 0x03-python_data_structures/0-print_list_integer.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/pythonii
-def print_list_integer(my_list=[]):
-    for integer in my_list:
-        print('{:d}' .format(integer))
--- 0x03-python_data_structures/5-no_c.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-def no_c(my_string):
-    llist = []
-    for letter in my_string:
-        if letter not in "cC":
-            llist.append(letter)
-    return "".join(llist)
